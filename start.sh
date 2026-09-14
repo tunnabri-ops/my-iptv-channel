ce4aa560df51d04fabd0a47aaca05cf8e14c3d1e
rahman <tunnabri@gmail.com>
2026-09-14 21:07:38 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,13 @@
 # HLS ফোল্ডার তৈরি
 mkdir -p /usr/share/nginx/html/live
 
-# Nginx ওয়েব সার্ভার ব্যাকগ্রাউন্ডে রান
+# Nginx চালু
 nginx
 
-# FFmpeg দিয়ে ২৪/৭ লুপ লাইভ স্ট্রিম
+# লো-সিপিইউ ও লো-লেটেন্সি বাফার-ফ্রি লুপ
 ffmpeg -re -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -stream_loop -1 -i /app/playlist.txt \
-  -c:v libx264 -preset veryfast -b:v 1500k -maxrate 1500k -bufsize 3000k \
-  -c:a aac -b:a 128k -ar 44100 \
-  -f hls -hls_time 4 -hls_list_size 5 -hls_flags delete_segments \
+  -c:v libx264 -preset ultrafast -tune zerolatency -b:v 800k -maxrate 900k -bufsize 1600k \
+  -vf "scale=854:480" \
+  -c:a aac -b:a 96k -ar 44100 \
+  -f hls -hls_time 2 -hls_list_size 3 -hls_flags delete_segments+split_by_time \
   /usr/share/nginx/html/live/stream.m3u8
